1d27652de226832ccbf7fc8fcd0c31c60ec9a4d8
taizo122 <pocketspla@gmail.com>
2022-04-19 01:11:41 +0900

    update

--- setup1.sh
+++ setup1.sh
@@ -11,7 +11,8 @@ yes | sudo apt update
 yes | sudo apt upgrade
 
 # パッケージのインストール
-yes | sudo apt install zsh git vim neovim openssh-server tmux tree curl
+yes | sudo apt install zsh git vim neovim openssh-server tmux tree curl snap
+yes | sudo snap install lsd
 
 # その他リポジトリなどをインストール
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
