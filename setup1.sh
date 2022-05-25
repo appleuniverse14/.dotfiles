a1863f15a7303ca4ce476a0d6aea61e405060c89
taizo122 <pocketspla@gmail.com>
2022-05-25 21:16:08 +0900

    update

--- setup1.sh
+++ setup1.sh
@@ -13,7 +13,8 @@ yes | sudo apt update
 yes | sudo apt upgrade
 
 # パッケージのインストール
-yes | sudo apt install zsh git wget vim-gtk neovim openssh-server tmux tree curl cargo python3-pip xsel xclip nodejs npm ripgrep fonts-ricty-diminished
+yes | sudo apt install zsh git wget vim-gtk neovim openssh-server tmux tree curl cargo python3-pip xsel xclip nodejs nodejs-dev node-gyp libssl1.0-dev fonts-ricty-diminished
+yes | sudo apt install npm
 cargo install lsd
 pip3 install pynvim
 
@@ -27,6 +28,12 @@ sudo npm install -g yarn
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+# dein.vim
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
 # Google Chromeのインストール
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
